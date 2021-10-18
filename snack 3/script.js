21e38f0d776736d007316564ca36febc81dfdf48
Alessia9802 <alessiamisciagna@yahoo.it>
2021-10-18 16:48:29 +0200

    esercizio 3

--- snack 3/script.js
+++ snack 3/script.js
@@ -1,8 +1,9 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
-let x = 1;
-
-for (let x = 1; x >= 10; x++) {
+const arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var length = arr.length;
+for (var i = 0; i < length; i++) {
   const askNumber = parseInt(prompt("Inserisci un numero"));
+  console.log(askNumber);
 }
