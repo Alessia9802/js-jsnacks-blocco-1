a6f06a1fa71942d5d9bf07bc41523f5c445bfa70
Alessia9802 <alessiamisciagna@yahoo.it>
2021-10-18 15:07:44 +0200

    Non riesco esercizio snack 3

---
+++ snack 3/script.js
@@ -0,0 +1,9 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+let play = true;
+
+while (play) {
+  const element = prompt("Inserisci un numero");
+  console.log(element);
+}
