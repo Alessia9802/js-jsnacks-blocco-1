8bfff9e6408872cd2bdcd9baefc8563e916f2a65
Alessia9802 <alessiamisciagna@yahoo.it>
2021-10-18 17:42:28 +0200

    esercizio 6

---
+++ snack 6/main.js
@@ -0,0 +1,9 @@
+/*
+Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente.
+ */
+
+const numero = prompt("Inserisci un numero");
+
+const cubo = Math.pow(numero, 3);
+console.log(numero, cubo);
